c8178d8f6b484f07542d66aeee84fa19f56fca9b
LeLoc-A23067 <ltloca23067@cusc.ctu.edu.vn>
2024-11-19 08:21:53 +0700

    commit2

--- config/database.config.js
+++ config/database.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/users');
+        await mongoose.connect('mongodb+srv://ltloca23067:loc5b2005@ltloc.e3us4.mongodb.net/?retryWrites=true&w=majority&appName=ltloc');
         console.log('Kết nối MongoDB thành công');
     } catch (err) {
         console.error('Lỗi kết nối MongoDB:', err);
